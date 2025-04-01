7d4011f76da9569ed767929b6f6d3d2b987c1c78
mcllivia <whois.mari.work@gmail.com>
2025-04-01 11:10:07 -0300

    Add files via upload
    continuação aula 3

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const userService = require ('./userService');
+
+const app = express (); //nome qualquer para express
+app.use(express.json());// vou habilitar json express
+
+// rota para criar usuário
+
+app.post("/users", async (req, res) => {
+    try {
+    
+    const {nome,email,senha,endereço,telefone,cpf}= req.body;
+    if (!nome|| !email|| !senha || !endereço || !telefone || !cpf){
+        return res.status(400).json 
+        ( {error:"Nome, email, endereço, senha, telefone e CPF são obrigatórios"})
+    }
+
+    const user = await userService.addUser(nome,email,senha,endereço,telefone,cpf);
+    res.status(200).json({user});
+} catch (erro){
+    console.log("Erro ao criar usuario", erro);
+    res.status(400).json({error: erro.message});
+}
+});
+
+//rota para listar todos os usuários
+
+app.get("/users", (req,res) => {
+    res.json(userService.getUsers());
+});
+
+//rota para excluir um usuario pelo id
+app.delete("/users/:id",(req,res) =>{
+    const id = parseInt (req.params.id); // converte o ID para número
+    try {
+        const resultado = userService.deleteUser(id); // tenta excluir o usuario
+        res.status(200).json(resultado); // retorna a mensagem de sucesso
+
+    } catch (erro){
+        res.status(404).json({ error: erro.message}); // retorna a mensaguem de erro
+    }
+});
+
+//rota para alterar usuario 
+app.put("/users/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const {nome,email,senha,endereço,telefone,cpf} = req.body;
+    try {
+        const user = userService.putUser(id,nome,email,senha,endereço,telefone,cpf);
+        res.status(200).json(user);
+    } catch (erro){
+        res.status(404).json({error: erro.message});
+    }
+});
+
+const port = 3000;
+app.listen(port,() => {
+    console.log("Servidor rodando na porta", port);
+})
